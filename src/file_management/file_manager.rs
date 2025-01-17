0c4eed10b006f68cf69de2613edc918fa33820f9
Janek Schleicher <hanfried@posteo.net>
2025-01-17 10:17:08 +0100

    Error handling with Result types instead of unwrapping everywhere

--- src/file_management/file_manager.rs
+++ src/file_management/file_manager.rs
@@ -15,79 +15,81 @@ pub struct FileManager {
 }
 
 impl FileManager {
-    pub fn new(db_directory: String, block_size: usize) -> Self {
+    pub fn new(db_directory: String, block_size: usize) -> Result<Self, std::io::Error> {
         let db_root: &Path = Path::new(db_directory.as_str());
         if !db_root.exists() {
             info!("Create db root: {:?}", db_root);
-            fs::create_dir(db_root).unwrap();
+            fs::create_dir(db_root)?;
         }
 
-        let temp_files: Vec<PathBuf> = fs::read_dir(db_root)
-            .unwrap()
+        let temp_files: Vec<PathBuf> = fs::read_dir(db_root)?
             .filter(|r| r.is_ok())
             .map(|r| r.unwrap().path())
             .filter(|p| p.starts_with("temp"))
             .collect();
         if !temp_files.is_empty() {
             info!("Delete temp files: {:?}", temp_files);
-            temp_files.iter().for_each(|p| fs::remove_file(p).unwrap());
+            for t in temp_files {
+                fs::remove_file(t)?;
+            }
         }
 
-        FileManager {
+        Ok(FileManager {
             db_directory,
             block_size,
             open_files: HashMap::new(),
-        }
+        })
     }
 
-    pub fn get_file(&mut self, filename: &str) -> &File {
+    pub fn get_file(&mut self, filename: &str) -> Result<&File, std::io::Error> {
         if !self.open_files.contains_key(filename) {
             let f = OpenOptions::new()
                 .read(true)
                 .write(true)
                 .create(true)
                 .truncate(true)
-                .open(Path::new(self.db_directory.as_str()).join(filename))
-                .unwrap();
+                .open(Path::new(self.db_directory.as_str()).join(filename))?;
             self.open_files.insert(filename.to_string(), f);
         }
-        self.open_files.get(filename).unwrap()
+        Ok(self.open_files.get(filename).unwrap())
     }
 
     // TODO: Synchronize
-    pub fn read(&mut self, block: &BlockId, page: &mut Page) {
+    pub fn read(&mut self, block: &BlockId, page: &mut Page) -> Result<(), std::io::Error> {
         let block_size = self.block_size;
         let seek_from = std::io::SeekFrom::Start((block.block_number * block_size) as u64);
-        let mut file = self.get_file(block.filename);
-        file.seek(seek_from).unwrap();
+        let mut file = self.get_file(block.filename)?;
+        file.seek(seek_from)?;
         let mut buf: Vec<u8> = vec![0; block_size];
-        file.read_exact(buf.as_mut_slice()).unwrap();
+        file.read_exact(buf.as_mut_slice())?;
         page.set_contents(buf.as_slice());
+        Ok(())
     }
 
     // TODO: Synchronize
-    pub fn write(&mut self, block: &BlockId, page: Page) {
+    pub fn write(&mut self, block: &BlockId, page: Page) -> Result<(), std::io::Error> {
         let seek_from = std::io::SeekFrom::Start((block.block_number * self.block_size) as u64);
-        let mut file = self.get_file(block.filename);
-        file.seek(seek_from).unwrap();
-        file.write_all(page.get_contents()).unwrap();
+        let mut file = self.get_file(block.filename)?;
+        file.seek(seek_from)?;
+        file.write_all(page.get_contents())?;
+        Ok(())
     }
 
-    fn block_length(&mut self, filename: &str) -> usize {
-        let mut file = self.get_file(filename);
-        let eof_offset = file.seek(std::io::SeekFrom::End(0)).unwrap();
-        eof_offset as usize / self.block_size
+    fn block_length(&mut self, filename: &str) -> Result<usize, std::io::Error> {
+        let mut file = self.get_file(filename)?;
+        let eof_offset = file.seek(std::io::SeekFrom::End(0))?;
+        Ok(eof_offset as usize / self.block_size)
     }
 
     // TODO: Synchronize
-    pub fn append<'a>(&mut self, filename: &'a str) -> BlockId<'a> {
+    pub fn append<'a>(&mut self, filename: &'a str) -> Result<BlockId<'a>, std::io::Error> {
         let block = BlockId {
             filename,
-            block_number: self.block_length(filename),
+            block_number: self.block_length(filename)?,
         };
         let seek_from = std::io::SeekFrom::Start((block.block_number * self.block_size) as u64);
-        let mut file = self.get_file(filename);
-        file.seek(seek_from).unwrap();
-        block
+        let mut file = self.get_file(filename)?;
+        file.seek(seek_from)?;
+        Ok(block)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ fn main() {
     // env_logger::init();
 
     let block_size = 4096;
-    let mut file_manager = FileManager::new("/tmp/test".to_string(), block_size);
+    let mut file_manager = FileManager::new("/tmp/test".to_string(), block_size).unwrap();
     println!("{file_manager:?}");
 
     let block = BlockId {
@@ -60,14 +60,14 @@ fn main() {
     let s = page1.get_string(pos_string);
     let i = page1.get_i32(pos_int);
     println!("{page1:?} {s:?} {i:?}");
-    file_manager.write(&block, page1);
+    file_manager.write(&block, page1).unwrap();
 
     let mut page2 = Page::new(block_size);
-    file_manager.read(&block, &mut page2);
+    file_manager.read(&block, &mut page2).unwrap();
     let s = page2.get_string(pos_string);
     let i = page2.get_i32(pos_int);
     println!("{page2:?} {s:?} {i:?}");
 
-    let appended_block: BlockId = file_manager.append("testfile");
+    let appended_block: BlockId = file_manager.append("testfile").unwrap();
     println!("appended_block: {appended_block:?}");
 }
